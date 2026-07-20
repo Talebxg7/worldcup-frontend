6d2962b03c8601f58f4aa6ece811948fa55118a3
Talebxg7 <talibjarrar@email.com>
2026-07-20 22:29:46 +0300

    fix: add missing go_router import in LeagueChallengeBanner

--- lib/features/competitions/presentation/widgets/league_challenge_banner.dart
+++ lib/features/competitions/presentation/widgets/league_challenge_banner.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import '../../../../core/theme/app_colors.dart';
 import '../../../../core/localization/app_localizations.dart';
 import 'league_challenge_dialog.dart';
