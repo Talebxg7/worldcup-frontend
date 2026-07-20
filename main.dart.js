4ed8b8a0ca90173bb13aac8420f4c8313b57ae71
Talebxg7 <Talebxg7@users.noreply.github.com>
2026-07-20 17:48:56 +0000

    deploy: 294c88bf215b45233b2b1cb22052f9c417cfde41

--- main.dart.js
+++ main.dart.js
@@ -41399,7 +41399,7 @@ $S:3}
 A.be1.prototype={
 $1(a){var s=A.fe().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/77e2e94772b6eb43759e34ed1ad7da4674e19cab/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/83675ed27633283e7fc296c8bca22e841224c096/":s)+a},
 $S:37}
 A.a3D.prototype={
 gC(a){var s=this.a
